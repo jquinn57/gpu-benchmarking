52ae3f186f42376028bbd8c991d8b2a4830a2711
Jack Quinn <jack.c.quinn@gmail.com>
2024-02-24 16:41:45 -0800

    setup 8 yolov5 models to test

--- mx3_benchmark.py
+++ mx3_benchmark.py
@@ -11,6 +11,7 @@ import threading
 import queue
 from kasa_reader import KasaReader
 from memryx import AsyncAccl
+from memryx import Benchmark
 
 class MX3Benchmark():
     def __init__(self, settings):
@@ -39,12 +40,29 @@ class MX3Benchmark():
                 yield img
         else:
             for n in range(self.num_images):
-                img = np.random.random((self.resolution, self.resolution, 3)).astype(np.float32)
+                # img = np.random.random((self.resolution, self.resolution, 3)).astype(np.float32)
+                # On Yolov5s-leakyrelu-640x640 using zeros: FPS = 100 
+                # using random data FPS=80 -- surpisingly makes a big difference even though this runs in a seperate thread
+                img = np.zeros((self.resolution, self.resolution, 3), dtype=np.float32)
                 yield img
         
     def output_processor(self, *outputs):
         self.count += 1
 
+
+    def baseline_power_test(self, model_config):
+        dfp_filename = model_config['filename']
+        self.resolution = model_config['resolution']
+        if self.kasa_reader:
+            self.kasa_reader.start_reading()
+        else:
+            return 0
+        cnt = 0
+        for img in self.data_source():
+            cnt += 1
+
+        power_avg_kasa = int(round(self.kasa_reader.avg_recent_readings()))
+        return power_avg_kasa
 
     def run_test(self, model_config):
 
@@ -53,7 +71,7 @@ class MX3Benchmark():
         # Accelerate using the MemryX hardware
         dfp_filename = model_config['filename']
         self.resolution = model_config['resolution']
-        accl = AsyncAccl(dfp_filename)
+        accl = AsyncAccl(dfp_filename, chip_gen=3.1)
         #model_dir = os.path.dirname(dfp_filename)
         #dfp_basename = os.path.basename(dfp_filename)
         #onnx_filename = os.path.join(model_dir, 'model_0_' + dfp_basename.replace('.dfp', '_post.onnx'))
@@ -67,9 +85,13 @@ class MX3Benchmark():
         accl.connect_output(self.output_processor) # starts asynchronous execution of output processing callback
 
         self.count = 0
+        print(f'Start test of {dfp_filename}')
         t0 = time.perf_counter()
         accl.wait() # wait for the accelerator to finish execution
         t1 = time.perf_counter()
+        # undocumented method?
+        accl.shutdown()
+        print('Done')
 
         if self.kasa_reader:
             power_avg_kasa = int(round(self.kasa_reader.avg_recent_readings()))
@@ -79,9 +101,7 @@ class MX3Benchmark():
         dt = t1 - t0
         fps = self.count / dt
         inference_time_ms = 1000 * dt  / self.count
-        # TODO:
-        latency_ms = 0
-    
+        
         output = {}
         output['batch_size'] = 1
         output['inference_time_ms'] = inference_time_ms
@@ -90,7 +110,7 @@ class MX3Benchmark():
         output['pcie_power'] = 0
         output['count'] = self.count
         output['total_time_s'] = dt
-        output['latency_ms'] = latency_ms
+        output['latency_ms'] = 0
         pprint.pprint(output)
         return output
     
@@ -112,13 +132,22 @@ def main():
     mx3_benchmark = MX3Benchmark(config['settings'])
 
     outputs = ['Model, Resolution, Batch Size, FPS, Latency(ms), PCIe Power, System Power']
+
     for model in config['models']:
         print()
         print(model)
         model_config = config['models'][model]
         res = model_config['resolution']
         batch_size = 1
+        baseline_power = mx3_benchmark.baseline_power_test(model_config)
+        outputs.append(f"{model}, {res}, {0}, {0}, {0}, {0}, {baseline_power}")
+
+        print(f'Baseline power: {baseline_power}')
         data = mx3_benchmark.run_test(model_config)
+        
+        # Do a seperate latency measurement
+        with Benchmark(dfp=model_config['filename'], verbose=2, chip_gen=3.1) as bm:
+           _, data['latency_ms'], _ = bm.run(threading=False)
         outputs.append(f"{model}, {res}, {batch_size}, {data['fps']}, {data['latency_ms']}, {data['pcie_power']}, {data['system_power']}")
     
     mx3_benchmark.shutdown()
