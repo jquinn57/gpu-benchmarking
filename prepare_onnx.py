0be5950699ca463480595216c080965b6a999d6c
Jack Quinn <jack.c.quinn@gmail.com>
2024-08-30 19:06:11 -0700

    fixed dynamic batch size conversion

--- prepare_onnx.py
+++ prepare_onnx.py
@@ -14,7 +14,12 @@ def main():
     model_list = glob.glob(os.path.join(args.input_dir, '*.onnx'))
     for model_name in model_list:
         output_name = os.path.join(args.output_dir, os.path.basename(model_name))
-        onnx_graph = initialize(input_onnx_file_path=model_name,output_onnx_file_path=output_name,initialization_character_string='batch')
+        print('\n')
+        print(output_name)
+        # if disable_onnxsim is False then onnx simplify is run and can cause some problems
+        # In the yolov5 models there are Resize nodes which use the sizes input which includes batch size which gets simplfied down to a constant tensor
+        # before the batch size is changed to 'batch'
+        onnx_graph = initialize(input_onnx_file_path=model_name,output_onnx_file_path=output_name, initialization_character_string='batch', disable_onnxsim=True)
 
 
 if __name__ == "__main__":
