9d3b9397e822dbb94e2a65905cd4d641c421c032
Jack Quinn <jack.c.quinn@gmail.com>
2025-02-01 12:18:50 -0800

    add power measurements

--- run_trtexec.py
+++ run_trtexec.py
@@ -1,6 +1,10 @@
 import os
 import subprocess
+import argparse
+import yaml
+import pprint
 from google_sheet_api import GoogleSheetAPI
+from kasa_reader import KasaReader
 
 def get_model_list(root_dir):
     # select a subset for Sam
@@ -12,7 +16,7 @@ def get_model_list(root_dir):
 
     model_list = []
     for dirpath, dirnames, filenames in os.walk(root_dir):
-        if 'onnx_model_0_fp16.onnx' in filenames:
+        if 'onnx_model_0.onnx' in filenames:
             model_name = os.path.basename(dirpath)
             if model_name not in selected_models:
                 continue
@@ -24,16 +28,17 @@ def get_model_list(root_dir):
     return model_list
 
 
-def get_throughput(onnx_path):
+def get_throughput(onnx_path, kasa_reader):
 
     engine_path = onnx_path.replace('.onnx', '.engine')
     command = [
         "/usr/src/tensorrt/bin/trtexec",
         f"--onnx={onnx_path}",
-        "--fp16",
-        f"--saveEngine={engine_path}"
+        f"--fp16",
+        f"--loadEngine={engine_path}"
     ]
     
+    kasa_reader.start_reading()
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
     
     throughput_line = None
@@ -43,24 +48,46 @@ def get_throughput(onnx_path):
             throughput_line = line.strip()
     
     process.wait()
+    power_avg_kasa = kasa_reader.avg_recent_readings()
+
     fps = float(throughput_line.split(' ')[-2])
-    return fps
+    return fps, power_avg_kasa
 
 
-if __name__ == '__main__':
-    model_list = get_model_list('/ssd/yolobench')
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', help='Path to config yaml', default='run_trtexec.yaml')
+    args = parser.parse_args()
 
-    header = ['Model', 'FPS']
-    gsapi = GoogleSheetAPI('Jetson-Orin-Nano-Super+AGX32')
-    gsapi.open_worksheet('375192165')
+    with open(args.config) as fp:
+        config = yaml.safe_load(fp)
+    pprint.pprint(config)
+    
+    onnx_model_path = config['settings']['model_path']
+
+    model_list = get_model_list(onnx_model_path)
+
+    header = ['Model', 'FPS', 'Sys Power (W)']
+    gsapi = GoogleSheetAPI(config['settings']['google_sheet_name'])
+    gsapi.open_worksheet(config['settings']['google_sheet_tab'])
     gsapi.append_row(header)
+
+    kasa_reader = KasaReader(*config['settings']['kasa'])
 
     for model_name, model_path in model_list:
 
         print('\n')
         print(model_name)
         print(model_path)
-        fps = get_throughput(model_path)
-        row = [model_name, fps]
+        fps, power_avg_kasa = get_throughput(model_path, kasa_reader)
+        row = [model_name, fps, power_avg_kasa]
         print(fps)
         gsapi.append_row(row)
+
+
+    kasa_reader.stop_reading()
+
+
+
+if __name__ == '__main__':
+    main()
